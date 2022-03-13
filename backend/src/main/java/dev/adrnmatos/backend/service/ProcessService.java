bc766ba0f2510e263cd7ad460e82f7705a75c645
Adriano Matos <adrnmatos@gmail.com>
2022-03-13 14:20:09 -0400

    service steriotype issue handling

--- backend/src/main/java/dev/adrnmatos/backend/service/ProcessService.java
+++ backend/src/main/java/dev/adrnmatos/backend/service/ProcessService.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dev.adrnmatos.backend.dto.ProcessDto;
 import dev.adrnmatos.backend.dto.mapper.ProcessMapper;
 import dev.adrnmatos.backend.repository.ProcessRepository;
 
+@Service
 public class ProcessService {
 
     @Autowired ProcessRepository repository;
